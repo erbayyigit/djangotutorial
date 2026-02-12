cf838a16e4bb9b24194ff06958cae27764694228
Erbay Yigit <erbayyigit@gmail.com>
2026-02-11 22:05:56 -0500

    made couple of views including file serving

--- polls/urls.py
+++ polls/urls.py
@@ -4,5 +4,6 @@ from . import views #imports views.py file
 
 urlpatterns=[
     path("", views.index, name="index"),
-    path("chowder", views.chowder, name="dog"),
+    path("chowder/", views.chowder, name="dog"),
+    path("myfile/", views.serve_chowders_file, name="myfile")
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
-
+import os
 
 
 def index(request):
     return HttpResponse("Hello Chowder. You are at the poll index!")
 
 def chowder(request):
-    return HttpResponse("I am Chowder. Bark Bark Bark Bark. Be careful")
+    return HttpResponse("I am Chowder. Bark Bark Bark Bark. Be careful.")
+
+
+def serve_chowders_file(request): #open a text file in url
+    # Build the path to the file
+    file_path = os.path.join(os.path.dirname(__file__), 'static', 'polls', 'myfile.txt')
+    
+    with open(file_path, 'r') as f:
+        content = f.read()
+    
+    # Build the path to the file
+    return HttpResponse(content, content_type='text/plain')
+
+
+
+
+
+
